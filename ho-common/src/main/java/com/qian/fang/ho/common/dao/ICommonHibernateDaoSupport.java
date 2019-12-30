db84ac201228b2c3014ce4ce994c149fcb5a53a7
owen511 <zyhai99148@sina.com>
2019-12-30 10:10:53 +0800

    修改成泛型接口

--- ho-common/src/main/java/com/qian/fang/ho/common/dao/ICommonHibernateDaoSupport.java
+++ ho-common/src/main/java/com/qian/fang/ho/common/dao/ICommonHibernateDaoSupport.java
@@ -9,6 +9,8 @@
 
 package com.qian.fang.ho.common.dao;
 
+import java.util.List;
+
 /**
  * 公共数据访问接口.
  * @ClassName: ICommonHibernateDaoSupport
@@ -16,6 +18,49 @@ package com.qian.fang.ho.common.dao;
  * @author owen 于 2019年11月25日 下午2:00:59
  */
 
-public interface ICommonHibernateDaoSupport {
+public interface ICommonHibernateDaoSupport<T> {
+	
+	/**
+	 * 清空.
+	 */
+	void clear();
+	
+	/**
+	 * 清空.
+	 */
+	void flush();
+	
+	/**
+	 * 保存.
+	 * @param t
+	 */
+	void save(T t);
+	
+	/**
+	 * 保存(批量).
+	 * @param ts
+	 */
+	void save(List<T> ts);
+	
+	/**
+	 * 删除(批量).
+	 * @param ts
+	 * @return
+	 */
+	List<T> delete(List<T> ts);
+	
+	/**
+	 * 修改(批量).
+	 * @param ts
+	 * @return
+	 */
+	List<T> update(List<T> ts);
+	
+	/**
+	 * 修改.
+	 * @param t
+	 * @return
+	 */
+	T update(T t);
 
 }
--- ho-common/src/main/java/com/qian/fang/ho/common/dao/impl/CommonHibernateDaoSupportImpl.java
+++ ho-common/src/main/java/com/qian/fang/ho/common/dao/impl/CommonHibernateDaoSupportImpl.java
@@ -9,6 +9,9 @@
 
 package com.qian.fang.ho.common.dao.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 
 import com.qian.fang.ho.common.dao.ICommonHibernateDaoSupport;
@@ -18,8 +21,50 @@ import com.qian.fang.ho.common.dao.ICommonHibernateDaoSupport;
  * @ClassName: CommonHibernateDaoSupportImpl
  * @Description: Description of this class
  * @author owen 于 2019年11月25日 下午2:00:26
+ * @param <T>
  */
 
-public class CommonHibernateDaoSupportImpl extends HibernateDaoSupport implements ICommonHibernateDaoSupport{
+public class CommonHibernateDaoSupportImpl<T> extends HibernateDaoSupport implements ICommonHibernateDaoSupport<T>{
+
+	public void clear() {
+		this.getHibernateTemplate().clear();
+	}
+	
+	public void flush() {
+		this.getHibernateTemplate().flush();;
+	}
+
+	public void save(T t) {
+		this.getHibernateTemplate().save(t);
+	}
+
+	public void save(List<T> ts) {
+		for(T t : ts) {
+			this.getHibernateTemplate().save(t);
+		}
+	}
+
+	public List<T> delete(List<T> ts) {
+		this.clear();
+		this.flush();
+		this.getHibernateTemplate().deleteAll(ts);
+		return ts;
+	}
+
+	public List<T> update(List<T> ts) {
+		List<T> result = new ArrayList<T>();
+		this.clear();
+		this.flush();
+		for (T t : ts) {
+			this.getHibernateTemplate().update(t);
+			result.add(t);
+		}
+		return result;
+	}
+
+	public T update(T t) {
+		this.getHibernateTemplate().update(t);
+		return t;
+	}
 
 }
